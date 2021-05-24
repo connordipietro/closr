8f99546329a7f37f143563ebf5a2a870bdf784a5
mchl-frlg <mchl.frlg@gmail.com>
2021-05-24 12:35:14 -0400

    first two server routes complete

---
+++ server/models/company.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+
+const CompanySchema = new Schema({
+  name: { type: String, required: true },
+  owner: String,
+  // FRONT END PEOPLE: if you need 'phone' as a number from any endpoints, please tell us! It will be a string otherwise
+  phone: String,
+  city: String,
+  state: String,
+  deals: Array,
+  createdAt: { type: Date, required: true },
+  industry: String
+});
+
+module.exports = mongoose.model("Company", CompanySchema);
