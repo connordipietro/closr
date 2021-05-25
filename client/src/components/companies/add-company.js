0fdd3f6d866785a4283710084039516b93d6b7c8
Ian Holmes <iqholmes@gmail.com>
2021-05-25 15:13:47 -0400

    updated add-company after with new company reducer

--- client/src/components/companies/add-company.js
+++ client/src/components/companies/add-company.js
@@ -1,12 +1,42 @@
 import { useState } from "react";
-import {Modal, Button, Form} from 'react-bootstrap';
+import {Modal, Button} from 'react-bootstrap';
+import { useDispatch } from "react-redux";
+import { postNewCopmany } from '../../actions'
 
 function AddCompany() {
   const [show, setShow] = useState(false);
+  const [name, setName] = useState('');
+  const [owner, setOwner] = useState('');
+  const [phone, setPhone] = useState('');
+  const [city, setCity] = useState('');
+  const [state, setState] = useState('');
+  const [deals, setDeals] = useState([]);
+  const [createdAt, setCreatedAt] = useState(Date);
+  const [industry, setIndustry] = useState('');
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
 
+  const dispatch = useDispatch();
+
+  const handleCompanyAdd = (e) => {
+    e.preventDefault();
+
+      dispatch(
+        postNewCopmany({
+          name,
+          owner,
+          phone,
+          city,
+          state,
+          deals,
+          createdAt,
+          industry
+        })
+      )
+    }
+
+  function renderAddCompanyModal() {
   return (
     <>
       <Button variant="primary" onClick={handleShow}>
@@ -18,40 +48,113 @@ function AddCompany() {
           <Modal.Title>Add a new Company</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <Form>
-            <Form.Group controlId="formBasicEmail">
-              <Form.Label>Name</Form.Label>
-              <Form.Control type="text" placeholder="Enter Company Name" />
-              <br />
-              <Form.Label>Owner</Form.Label>
-              <Form.Control type="text" placeholder="Enter Company Owner" />
-              <br />
-              <Form.Label>Phone</Form.Label>
-              <Form.Control type="text" placeholder="Enter Company Phone Number" />
-              <br />
-              <Form.Label>City</Form.Label>
-              <Form.Control type="text" placeholder="Enter Company City" />
-              <br />
-              <Form.Label>State</Form.Label>
-              <Form.Control type="text" placeholder="Select Company State" />
-              <br />
-              <Form.Label>Industry</Form.Label>
-              <Form.Control type="text" placeholder="Enter Company Indsutry" />
-              <br />
-            </Form.Group>
-          </Form>
+          <form>
+          <div className="form-group">
+            <label>Name</label>
+            <input
+              required
+              className="form-control"
+              placeholder="Enter Company Name"
+              value={name}
+              onChange={(e) => setName(e.target.value)} 
+            ></input>
+          </div>
+          <br />
+          <div className="form-group">
+            <label>Owner</label>
+            <input
+              className="form-control"
+              placeholder="Enter Company Owner" 
+              value={owner}
+              onChange={(e) => setOwner(e.target.value)} 
+            ></input>
+          </div>
+          <br />
+
+          <div className="form-group">
+            <label>Phone</label>
+            <input
+              className="form-control"
+              placeholder="Enter Company Phone Number" 
+              value={phone}
+              onChange={(e) => setPhone(e.target.value)} 
+            ></input>
+          </div>
+          <br />
+
+          <div className="form-group">
+            <label>City</label>
+            <input
+              className="form-control"
+              placeholder="Enter Company City" 
+              value={city}
+              onChange={(e) => setCity(e.target.value)} 
+            ></input>
+          </div>
+          <br />
+
+          <div className="form-group">
+            <label>State</label>
+            <input
+              className="form-control"
+              placeholder="Enter Company State" 
+              value={state}
+              onChange={(e) => setState(e.target.value)} 
+            ></input>
+          </div>
+          <br />
+
+          <div className="form-group">
+            <label>Deals</label>
+            <input
+              className="form-control"
+              placeholder="Enter Company Deals" 
+              value={deals}
+              onChange={(e) => setDeals(e.target.value)} 
+            ></input>
+          </div>
+          <br />
+
+          <div className="form-group">
+            <label>Created Date</label>
+            <input
+              className="form-control"
+              placeholder="Enter Company Created Date" 
+              value={Date}
+              onChange={(e) => setCreatedAt(e.target.value)} 
+            ></input>
+          </div>
+          <br />
+
+          <div className="form-group">
+            <label>Industry</label>
+            <input
+              className="form-control"
+              placeholder="Enter Company Industy" 
+              value={industry}
+              onChange={(e) => setIndustry(e.target.value)} 
+            ></input>
+          </div>
+          <br />
+          </form>
         </Modal.Body>
         <Modal.Footer>
           <Button variant="secondary" onClick={handleClose}>
             Close
           </Button>
-          <Button variant="primary" onClick={handleClose}>
+          <Button variant="primary" onClick={handleCompanyAdd}>
             Save
           </Button>
         </Modal.Footer>
       </Modal>
     </>
   );
+  }
+  return (
+    <div>
+      {renderAddCompanyModal()}
+    </div>
+  )
 }
 
 export default AddCompany;
