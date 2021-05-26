762570e58e5cb1493e5417381282580c917cb1c4
Connor DiPietro <connor.dipietro@gmail.com>
2021-05-26 14:13:04 -0400

    removes unused useselector import from add-deal.js

--- client/src/components/deals/add-deal.js
+++ client/src/components/deals/add-deal.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import {Modal, Button, Form} from 'react-bootstrap';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { getDeals } from '../../actions'
 
 function AddDeal() {
