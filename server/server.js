b55d318d176c16c5a55ba0963788af32ad91a69c
unknown <joshkamrass@gmail.com>
2021-05-26 08:51:35 -0400

    deployment from root

--- server/server.js
+++ server/server.js
@@ -43,7 +43,7 @@ if (process.env.NODE_ENV === "production") {
   // if it doesn't recognize the route
   const path = require("path");
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve("parsity-crm", "client", "build", "index.html"));
+    res.sendFile(path.resolve("../client", "build", "index.html"));
   });
 }
 
