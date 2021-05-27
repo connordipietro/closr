bd607343369f9807837e0ffc4bd4cd2155bbe9e7
unknown <joshkamrass@gmail.com>
2021-05-27 05:42:51 -0400

    refactors server.js for consistent style

--- server/server.js
+++ server/server.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
-const keys = require('./config/keys');
-const http = require('http');
+const keys = require("./config/keys");
+const http = require("http");
+const companyRoutes = require("./routes/companyRoutes");
+const dealRoutes = require("./routes/dealRoutes");
 
 mongoose.connect(keys.MONGODB_URI, {
   useNewUrlParser: true,
@@ -17,7 +19,6 @@ app.use(
     extended: true,
   })
 );
-
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
@@ -28,15 +29,10 @@ app.use((req, res, next) => {
   next();
 });
 
-const companyRoutes = require("./routes/companyRoutes");
-const dealRoutes = require("./routes/dealRoutes")
-
 app.use(companyRoutes, dealRoutes);
-
 
 if (process.env.NODE_ENV === "production") {
   // Express will serve up production assets
-  // like our main.js file, or main.css file!
   app.use(express.static("client/build"));
 
   // Express will serve up the index.html file
@@ -47,14 +43,9 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-
 const PORT = process.env.PORT || 8000;
 
 const server = http.createServer(app);
 server.listen(PORT, () => {
-  console.log("Node.js listening on port " + PORT)
+  console.log("Node.js listening on port " + PORT);
 });
-
-// app.listen(PORT, () => {
-//   console.log("Node.js listening on port " + PORT);
-// });
