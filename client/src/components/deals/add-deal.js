40bb18a944802429a4da4b2e320348d2469ff89b
mchl-frlg <mchl.frlg@gmail.com>
2021-05-28 11:16:01 -0400

    with add deal dropdown menu, actions updated to post deal

--- client/src/components/deals/add-deal.js
+++ client/src/components/deals/add-deal.js
@@ -1,18 +1,17 @@
-import axios from "axios";
 import { useState, useEffect } from "react";
-import { Modal, Button, Form } from 'react-bootstrap';
+import {Modal, Button} from 'react-bootstrap';
 import { useDispatch } from "react-redux";
 import { postNewDeal } from '../../actions'
 import { useForm } from "react-hook-form";
+import axios from "axios";
 import { yupResolver } from '@hookform/resolvers/yup';
-import CompanyMenu from './company-menu'
 import * as Yup from 'yup';
 
 const dealSchema = Yup.object().shape({
   name: Yup.string().required("Please enter a name for the deal"),
   owner: Yup.string(),
-  amount: Yup.number().required().typeError("Please enter a deal amount"),
-  company: Yup.string().required("Please select a Company"),
+  amount: Yup.number().required("Please enter a deal amount"),
+  company: Yup.string()
 })
 
 function AddDeal() {
@@ -23,8 +22,10 @@ function AddDeal() {
   const dispatch = useDispatch(); 
   const [show, setShow] = useState(false);
   const [companiesList, setCompaniesList] = useState([]);
+  useEffect(() => axios.get("/companies/list").then(({ data }) => setCompaniesList(data)), [setCompaniesList])
 
-  const handleCompanyAdd = (data) => {
+  const handleDealAdd = (data) => {
+    debugger;
     dispatch(postNewDeal(data))
     reset()
     setShow(false);
@@ -35,75 +36,62 @@ function AddDeal() {
     reset()
   }
 
-  const getCompaniesList = async () => {
-    let list = await axios.get("/companies/list");
-    debugger;
-    setCompaniesList(list.data);
-  }
+  const formFields = ['Name', 'Owner', 'Amount']
 
-  useEffect(() => {
-    getCompaniesList();
-  }, [])
+  return (
+    <>
+      <Button variant="primary" className="add-button" onClick={() => setShow(true)}>
+        Add Deal
+      </Button>
 
-  // const handleChange = (e) => {
-  //   const { name, value, type, checked } = e.target;
-  //   const newValue = type === "checkbox" ? checked : value;
-  //   setFormInfo(prevState => ({...prevState, [name]: newValue}));
-  // };
-  const formFields = ['Name', 'Owner', 'Amount', 'Company']
-
-  const renderAddDealModal = () => {
-    return (
-      <>
-        <Button variant="primary" className="add-button" onClick={() => setShow(true)}>
-          Add a Deal
-        </Button>
-
-        <Modal show={show} onHide={() => setShow(false)}>
-          <Modal.Header>
-            <Modal.Title>Add a new Deal</Modal.Title>
-          </Modal.Header>
-          <form onSubmit={handleSubmit(handleCompanyAdd)}>
-            <Modal.Body>
-              {formFields.map(field => {
-                return (
-                  <div key ={field}>
-                    <div className="form-group" >
-                      <label>{field}</label>
-                      <input
-                        className="form-control"
-                        placeholder={`Enter Company ${field}`}
-                        name={field.toLowerCase()}
-                        {...register(field.toLowerCase())}
-                      ></input>
-                      {errors[field.toLowerCase()]?.message}
-                    </div>
-                    <br />
-                  </div>
-                )
-              })} 
-                <div key ="Company">
+      <Modal show={show} onHide={() => setShow(false)}>
+        <Modal.Header>
+          <Modal.Title>Add a new Company</Modal.Title>
+        </Modal.Header>
+        <form onSubmit={handleSubmit(handleDealAdd)}>
+          <Modal.Body>
+            {formFields.map(field => {
+              return (
+                <div key ={field}>
                   <div className="form-group" >
-                    <label>Company</label>
-                    <CompanyMenu companiesList={companiesList}/>
+                    <label>{field}</label>
+                    <input
+                      className="form-control"
+                      placeholder={`Enter Company ${field}`}
+                      name={field.toLowerCase()}
+                      {...register(field.toLowerCase())}
+                    ></input>
+                    {errors[field.toLowerCase()]?.message}
                   </div>
                   <br />
                 </div>
-            </Modal.Body>
-            <Modal.Footer>
-              <Button variant="secondary" onClick={onClose}>Close</Button>
-              <Button type="submit" variant="primary">Submit</Button>
-            </Modal.Footer>
-          </form>
-        </Modal>
-      </>
-    );
-  };
-
-  return (
-    <div>
-      {renderAddDealModal()}
-    </div>
+              )
+            })}
+            <div key ="company">
+              <div className="form-group" >
+                <label>Company</label>
+                <select 
+                  className="form-control"
+                  name="company"
+                  {...register("company")}
+                  >
+                  {companiesList.map(company => {
+                    return (
+                          <option key={company._id} value={company._id}>{company.name}</option>
+                    );
+                  })}
+                </select>
+              </div>
+              <br />
+            </div>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={onClose}>Close</Button>
+            <Button type="submit" variant="primary">Submit</Button>
+          </Modal.Footer>
+        </form>
+      </Modal>
+    </>
   );
 };
 
--- client/src/components/deals/company-menu.js
+++ client/src/components/deals/company-menu.js
@@ -1,16 +1,7 @@
 import { Modal, Button, Form, useDropdownMenu } from 'react-overlays';
 import styled from 'styled-components';
+import Dropdown from 'react-overlays/Dropdown'
 
-const MenuContainer = styled("div")`
-  display: ${(p) => (p.show ? "flex" : "none")};
-  min-width: 150px;
-  position: absolute;
-  z-index: 1000;
-  flex-direction: column;
-  border: 1px solid #e5e5e5;
-  background-color: white;
-  box-shadow: 0 5px 15px rgba(0, 0, 0, 0.5);
-`;
 
 const CompanyMenu = ({ role, companiesList }) => {
   const { show, onClose, props } = useDropdownMenu({
@@ -18,36 +9,23 @@ const CompanyMenu = ({ role, companiesList }) => {
     offset: [0, 8],
   });
   return (
-    <MenuContainer {...props} role={role} show={show}>
-      {companiesList.map(company => {
-        return (
-          <button
-            key={company}
-            type="button"
-            onClick={onClose}
-            className="text-left hover:bg-brand-100 px-6 py-2"
-          >
-            {company.name}
-          </button>
-        );
-      })}
-      
-      <button
-        type="button"
-        onClick={onClose}
-        className="text-left hover:bg-brand-100 px-6 py-2"
-      >
-        Item 1
-      </button>
-      <button
-        type="button"
-        onClick={onClose}
-        className="text-left hover:bg-brand-100 px-6 py-2"
-      >
-        Item 2
-      </button>
-    </MenuContainer>
+    <>
+      <div key ="Company">
+        <div className="form-group" >
+          <label>Company</label>
+          <select className="form-control" id="sel1">
+            {companiesList.map(company => {
+              return (
+                    <option key={company.id}>{company.name}</option>
+              );
+            })}
+          </select>
+        </div>
+        <br />
+      </div>
+    </>
   );
 };
 
 export default CompanyMenu
+                  
