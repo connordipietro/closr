e9e6c82937ccd6a7395850bd17636e10dec5712b
abeakaid <74392388+abeK021@users.noreply.github.com>
2021-05-30 14:25:17 -0400

    button effects, navbar responsive scaling

--- client/src/components/nav/nav.js
+++ client/src/components/nav/nav.js
@@ -1,31 +1,24 @@
-/* import { Link } from "react-router-dom"; */
+import { Link } from "react-router-dom"; 
 import "./nav-style.css";
 import iconLogo from './icon-logo.png'
 
 function Nav() {
   return (
+    
     <nav class="navbar navbar-expand-sm ">
       <img className="icon-logo" src={iconLogo}></img>
-    <h3 className="nav-title">Parsity-CRM </h3>
+    <Link className="nav-title navbar-brand" to="/companies"><h3 className="nav-title">Parsity-CRM </h3></Link> 
+   
     <ul class="navbar-nav">
         <li class="nav-item">
             <a class="nav-link" href="#">
               <button className="dashboard-button">Dashboard</button>
             </a>
         </li>
-        <li class="nav-item">
-            <a class="nav-link" href="#">
-              Contacts
-            </a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="#">
-              Settings
-            </a>
-        </li>
+          <li className="login-signup nav-item"><h4 >Login</h4></li>
+          <li className="login-signup nav-item"><h4 >Sign Up</h4></li>
     </ul>
 </nav>
-
 
   );
 };
