cca07f65c176e8a017853b46b809cb33a3ecb491
unknown <joshkamrass@gmail.com>
2021-06-01 17:19:58 -0400

    completes the revenue by company endpoint

--- server/routes/dashboardRoutes.js
+++ server/routes/dashboardRoutes.js
@@ -25,18 +25,36 @@ router.get("/conversion-percentage-overall", (req, res) => {
 
 // Provides list of the revenue from the deals for each company (To-Do: Limit it to the last six months)
 router.get("/sales-by-company", (req, res) => {
-  Deal.find({stage: "Closed Won"}).populate({path: "company"}).exec()
-    .then(dealsWon => {
-      const salesRevenueByCompany = dealsWon.reduce((acc, deal) => {
-        acc[deal.company.name] = acc[deal.company.name] ? acc[deal.company.name] + deal.amount : deal.amount;
-        return acc;
-      }, {});
-      res.send(salesRevenueByCompany);
+  Deal.aggregate([
+    { $match: {archived: true, stage: "Closed Won"}},
+    { $group: { _id: "$company", total: {$sum: "$amount"}}},
+    { $sort: {total: -1 }},
+    { $limit: 5},
+    { $lookup: {from: 'companies', localField: '_id', foreignField: '_id', as: 'company'}},
+    { $unwind: "$company"},
+    { $project: {total: 1, name: "$company.name"}},
+  ])
+    .then(deals => {
+      res.send(deals)
     })
     .catch(err => {
-      console.error(err);
+      console.error(err)
       res.end();
     })
+
+
+  // Deal.find({archived: true, stage: "Closed Won"}).populate({path: "company"}).exec()
+  //   .then(dealsWon => {
+  //     const salesRevenueByCompany = dealsWon.reduce((acc, deal) => {
+  //       acc[deal.company.name] = acc[deal.company.name] ? acc[deal.company.name] + deal.amount : deal.amount;
+  //       return acc;
+  //     }, {});
+  //     res.send(salesRevenueByCompany);
+  //   })
+  //   .catch(err => {
+  //     console.error(err);
+  //     res.end();
+  //   })
 })
 
 module.exports = router;
