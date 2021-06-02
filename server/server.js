5cb2cf1c59062d789c0c04ee1066c72f1b2421d1
Connor DiPietro <connor.dipietro@gmail.com>
2021-06-02 10:39:45 -0400

    deployment testing

--- server/server.js
+++ server/server.js
@@ -30,13 +30,13 @@ app.use(allRoutes);
 
 if (process.env.NODE_ENV === "production") {
   // Express will serve up production assets
-  app.use(express.static("client/build"));
+  app.use(express.static(path.join(__dirname, '../client/build')));
 
   // Express will serve up the index.html file
   // if it doesn't recognize the route
   const path = require("path");
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve("client", "build", "index.html"));
+    res.sendFile(path.join(__dirname, "../client/build/index.html"));
   });
 }
 
