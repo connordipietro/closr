bb0d72cec8223cb018d216ef99b25189eb1a79c4
Connor DiPietro <connor.dipietro@gmail.com>
2021-06-04 09:32:14 -0400

    adds front end comments, updates index.html

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -11,14 +11,14 @@ import DashboardContainer from './dashboard/dashboard-container';
 function App() {
   return (
     <>
-      <Nav />
+      <Nav /> {/* Nav bar */}
       <Switch>
         <Route path="/companies/:_id" component={CompanyView} />
         <Route path="/companies" component={CompaniesContainer} />
         <Route path="/deals/:_id" component={DealView} />
         <Route path="/deals" component={DealsContainer} />
         <Route path="/dashboard" component={DashboardContainer} />
-        <Redirect to="/companies" />
+        <Redirect to="/dashboard" /> {{/* Default landing page */}}
       </Switch>
     </>
   );
